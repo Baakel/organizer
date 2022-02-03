32af62e41201fd97690842e98938fae832523cab
Núnez Navarro <diego.nunez@iosb.fraunhofer.de>
2022-02-03 16:00:11 +0100

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>Copyright Xibalba 2022</p>
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<title>Organizer</title>
+
+<div class="p-8 m-0 h-full text-center">
+    <h1 class="text-center text-6xl text-white mb-10">Today's goal</h1>
+    <input type="text" class="w-11/12" placeholder="">
+    <p class="text-red-600">Testing this other paragraph</p>
+</div>
