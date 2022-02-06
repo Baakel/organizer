79eebeb147cf1cf49efbbfc7a565e17419c282b4
Núnez Navarro <diego.nunez@iosb.fraunhofer.de>
2022-02-06 23:44:56 +0100

    Working firebase backend connection

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,6 @@
 	<slot />
 </main>
 
-<footer>
+<footer class="text-center text-gray-600">
 	<p>Copyright Xibalba 2022</p>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+    import Input from "$lib/input.svelte";
+    //import { auth, provider } from "$lib/_db"
+    //import { driver, session } from "$lib/_neo4jdb"
+    //import { auth } from "$lib/auth";
+    import {doLogin} from "$lib/login"
+
+    export let uid;
+    export let displayName;
+
+</script>
+
 <title>Organizer</title>
 
 <div class="p-8 m-0 h-full text-center">
     <h1 class="text-center text-6xl text-white mb-10">Today's goal</h1>
-    <input type="text" class="w-11/12" placeholder="">
+    <Input placeholder="  What do you want to accomplish today?"/>
     <p class="text-red-600">Testing this other paragraph</p>
+    <p class="text-white">Your user id is {uid} and your display name is {displayName}</p>
+
+    <button on:click={() => {doLogin()}}> Sign in with google</button>
+    <button>Sign out</button>
 </div>
