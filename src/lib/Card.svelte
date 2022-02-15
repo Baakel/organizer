5a9f09b19d9dcbe831fb71ba3ef930346eecc153
Núnez Navarro <diego.nunez@iosb.fraunhofer.de>
2022-02-15 01:38:59 +0100

    Included Card

---
+++ src/lib/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+<div class="my-4 bg-neutral-800 rounded-2xl shadow-[1px_1px_2px_0_rgb(17,19,30),-1px_-1px_2px_0_rgb(53,57,84)]">
+    <slot name="title"></slot>
+    <hr class="border-t-[#0e1018] border-b-[#2d3150] border-y-[0.8px] mx-14 my-2">
+    <slot name="content"></slot>
+    {#if $$slots.footer}
+        <hr class="border-t-[#0e1018] border-b-[#2d3150] border-y-[0.8px] mx-14 my-2">
+        <slot name="footer"></slot>
+    {/if}
+</div>
